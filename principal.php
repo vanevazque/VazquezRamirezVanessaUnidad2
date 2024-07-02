9ab599edaf31d8d9576dda6583c4659a81e233d8
Sebas170423 <132617418+Sebas170423@users.noreply.github.com>
2024-07-02 17:11:27 -0600

    se agrego la pagina principal

---
+++ principal.php
@@ -0,0 +1,521 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+        
+        <title>Venta</title>
+
+       
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+		<link rel="stylesheet" href="assets/css/linearicons.css">
+
+        <link rel="stylesheet" href="assets/css/animate.css">
+
+        <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+		<link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+		
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		
+		<link rel="stylesheet" href="assets/css/bootsnav.css" >	
+        
+        <link rel="stylesheet" href="assets/css/style.css">
+        
+        <link rel="stylesheet" href="assets/css/responsive.css">
+
+    </head>
+	
+	<body>
+		
+		
+	
+		<header id="home" class="welcome-hero">
+
+			<div id="header-carousel" class="carousel slide carousel-fade" data-ride="carousel">
+				 <ol class="carousel-indicators">
+					<li data-target="#header-carousel" data-slide-to="0" class="active"><span class="small-circle"></span></li>
+					<li data-target="#header-carousel" data-slide-to="1"><span class="small-circle"></span></li>
+					<li data-target="#header-carousel" data-slide-to="2"><span class="small-circle"></span></li>
+				</ol><!-- /ol-->
+
+				<div class="carousel-inner" role="listbox">
+					<div class="item active">
+						<div class="single-slide-item slide1">
+							<div class="container">
+								<div class="welcome-hero-content">
+									<div class="row">
+										<div class="col-sm-7">
+											<div class="single-welcome-hero">
+												<div class="welcome-hero-txt">
+													<h4>Mas buscado</h4>
+													<h2>Balon de futbol telstar 2018</h2>
+													
+													<div class="packages-price">
+														<p>
+															$ 1500
+															<del>$ 2300</del>
+														</p>
+													</div>
+													<button class="btn-cart welcome-add-cart" onclick="window.location.href='#'">
+														<span class="lnr lnr-plus-circle"></span>
+														añadir <span></span>
+													</button>
+													
+												</div><!--/.welcome-hero-txt-->
+											</div><!--/.single-welcome-hero-->
+										</div><!--/.col-->
+										<div class="col-sm-5">
+											<div class="single-welcome-hero">
+												<div class="welcome-hero-img">
+													<img src="assets/images/slider/telstar.jpg" alt="slider image">
+												</div><!--/.welcome-hero-txt-->
+											</div><!--/.single-welcome-hero-->
+										</div><!--/.col-->
+									</div><!--/.row-->
+								</div><!--/.welcome-hero-content-->
+							</div><!-- /.container-->
+						</div><!-- /.single-slide-item-->
+
+					</div><!-- /.item .active-->
+
+					<div class="item">
+						<div class="single-slide-item slide2">
+							<div class="container">
+								<div class="welcome-hero-content">
+									<div class="row">
+										<div class="col-sm-7">
+											<div class="single-welcome-hero">
+												<div class="welcome-hero-txt">
+													<h4>Mas buscado</h4>
+													<h2>adidas Jabulani 2010</h2>
+													
+													<div class="packages-price">
+														<p>
+															$ 1700.00
+															<del>$ 2000.00</del>
+														</p>
+													</div>
+													<button class="btn-cart welcome-add-cart" onclick="window.location.href='#'">
+														<span class="lnr lnr-plus-circle"></span>
+														añadir <span></span>
+													</button>
+													
+												</div><!--/.welcome-hero-txt-->
+											</div><!--/.single-welcome-hero-->
+										</div><!--/.col-->
+										<div class="col-sm-5">
+											<div class="single-welcome-hero">
+												<div class="welcome-hero-img">
+													<img src="assets/images/slider/jabulani.jpg" alt="slider image">
+												</div><!--/.welcome-hero-txt-->
+											</div><!--/.single-welcome-hero-->
+										</div><!--/.col-->
+									</div><!--/.row-->
+								</div><!--/.welcome-hero-content-->
+							</div><!-- /.container-->
+						</div><!-- /.single-slide-item-->
+
+					</div><!-- /.item .active-->
+
+					<div class="item">
+						<div class="single-slide-item slide3">
+							<div class="container">
+								<div class="welcome-hero-content">
+									<div class="row">
+										<div class="col-sm-7">
+											<div class="single-welcome-hero">
+												<div class="welcome-hero-txt">
+													<h4>Mas buscado</h4>
+													<h2>Balon nike mercural fade</h2>
+													
+													<div class="packages-price">
+														<p>
+															$ 2000.00
+															<del>$ 2300.00</del>
+														</p>
+													</div>
+													<button class="btn-cart welcome-add-cart" onclick="window.location.href='#'">
+														<span class="lnr lnr-plus-circle"></span>
+														añadir <span></span>
+													</button>
+										
+												</div><!--/.welcome-hero-txt-->
+											</div><!--/.single-welcome-hero-->
+										</div><!--/.col-->
+										<div class="col-sm-5">
+											<div class="single-welcome-hero">
+												<div class="welcome-hero-img">
+													<img src="assets/images/slider/mercurial.jpg" alt="slider image">
+												</div><!--/.welcome-hero-txt-->
+											</div><!--/.single-welcome-hero-->
+										</div><!--/.col-->
+									</div><!--/.row-->
+								</div><!--/.welcome-hero-content-->
+							</div><!-- /.container-->
+						</div><!-- /.single-slide-item-->
+						
+					</div><!-- /.item .active-->
+				</div><!-- /.carousel-inner-->
+
+			</div><!--/#header-carousel-->
+
+			<!-- top-area Start -->
+			<div class="top-area">
+				<div class="header-area">
+					<!-- Start Navigation -->
+				    <nav class="navbar navbar-default bootsnav  navbar-sticky navbar-scrollspy"  data-minus-value-desktop="70" data-minus-value-mobile="55" data-speed="1000">
+
+				        <!-- Start Top Search -->
+				        <div class="top-search">
+				            <div class="container">
+				                <div class="input-group">
+				                    <span class="input-group-addon"><i class="fa fa-search"></i></span>
+				                    <input type="text" class="form-control" placeholder="Search">
+				                    <span class="input-group-addon close-search"><i class="fa fa-times"></i></span>
+				                </div>
+				            </div>
+				        </div>
+				        <!-- End Top Search -->
+
+				        <div class="container">            
+				            <!-- Start Atribute Navigation -->
+				            <div class="attr-nav">
+				                <ul>
+				                	<li class="search">
+				                		<a href="#"><span class="lnr lnr-magnifier"></span></a>
+				                	</li><!--/.search-->
+				                	
+				                	</li><!--/.search-->
+				                    <li class="dropdown">
+				                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" >
+				                            <span class="lnr lnr-cart"></span>
+											<span class="badge badge-bg-1"></span>
+				                        </a>
+				                        <ul class="dropdown-menu cart-list s-cate">
+				                      
+				                           
+				                            <li class="single-cart-list">
+				                                <a href="#" class="photo"><img src="assets/images/slider/telstar.jpg" class="cart-thumb" alt="image" /></a>
+				                                <div class="cart-list-txt">
+				                                	<h6><a href="#">Telstar 2018<br></a></h6>
+				                                	<p>1 x - <span class="price">$1500.00</span></p>
+				                                </div><!--/.cart-list-txt-->
+				                                <div class="cart-close">
+				                                	<span class="lnr lnr-cross"></span>
+				                                </div><!--/.cart-close-->
+				                            </li><!--/.single-cart-list -->
+				                            <li class="total">
+				                                <span>Total: $1500.00</span>
+				                                <button class="btn-cart pull-right" onclick="window.location.href='#'">ver carrito</button>
+				                            </li>
+				                        </ul>
+				                    </li><!--/.dropdown-->
+				                </ul>
+				            </div><!--/.attr-nav-->
+				            <!-- End Atribute Navigation -->
+
+				            <!-- Start Header Navigation -->
+				            <div class="navbar-header">
+				                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
+				                    <i class="fa fa-bars"></i>
+				                </button>
+				                <a class="navbar-brand" href="#"><span>Venta de ropa</span></a>
+
+				            </div><!--/.navbar-header-->
+				            <div class="collapse navbar-collapse menu-ui-design" id="navbar-menu">
+				                <ul class="nav navbar-nav navbar-center" data-in="fadeInDown" data-out="fadeOutUp">
+				                	<li class=""><a href="login.php">Ingresar</a></li>
+				                    <li class=""><a href="registro.php">Crear cuenta</a></li>
+				                </ul><!--/.nav -->
+				            </div><!-- /.navbar-collapse -->
+				        </div><!--/.container-->
+				    </nav><!--/nav-->
+				</div><!--/.header-area-->
+			    <div class="clearfix"></div>
+
+			</div><!-- /.top-area-->
+			<!-- top-area End -->
+
+		</header><!--/.welcome-hero-->
+		<!--welcome-hero end -->
+
+		<!--populer-products start -->
+		<section id="populer-products" class="populer-products">
+			<div class="container">
+				<div class="populer-products-content">
+					<div class="row">
+						<div class="col-md-3">
+							<div class="single-populer-products">
+								<div class="single-populer-product-img mt40">
+									<img src="assets/images/populer-products/p1.png" alt="populer-products images">
+								</div>
+								<h2><a href="#">Sofa</a></h2>
+								<div class="single-populer-products-para">
+									<p>Color cafe muy comodo para una persona</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="single-populer-products">
+								<div class="single-inner-populer-products">
+									<div class="row">
+										<div class="col-md-4 col-sm-12">
+											<div class="single-inner-populer-product-img">
+												<img src="assets/images/populer-products/p2.png" alt="populer-products images">
+											</div>
+										</div>
+										<div class="col-md-8 col-sm-12">
+											<div class="single-inner-populer-product-txt">
+												<h2>
+													<a href="#">
+														Silla Sofa<span></span>
+													</a>
+												</h2>
+												<p>
+													silla  muy comoda 
+												</p>
+												<div class="populer-products-price">
+													<h4>A la venta por <span>$1300.00</span></h4>
+												</div>
+												<button class="btn-cart welcome-add-cart populer-products-btn" onclick="window.location.href='#'">
+													Ver mas
+												</button>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="single-populer-products">
+								<div class="single-populer-products">
+									<div class="single-populer-product-img">
+										<img src="assets/images/populer-products/p3.png" alt="populer-products images">
+									</div>
+									<h2><a href="#">lampara metalica</a></h2>
+									<div class="single-populer-products-para">
+										<p>lampara metalica color cafe</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div><!--/.container-->
+
+		</section><!--/.populer-products-->
+		<!--populer-products end-->
+
+		<!--new-arrivals start -->
+		<section id="new-arrivals" class="new-arrivals">
+			<div class="container">
+				<div class="section-header">
+					<h2>Ofertas</h2>
+				</div><!--/.section-header-->
+				<div class="new-arrivals-content">
+					<div class="row">
+						<div class="col-md-3 col-sm-4">
+							<div class="single-new-arrival">
+								<div class="single-new-arrival-bg">
+									<img src="assets/images/slider/predator.jpg" alt="new-arrivals images">
+									<div class="single-new-arrival-bg-overlay"></div>
+									
+									<div class="new-arrival-cart">
+										<p>
+											<span class="lnr lnr-cart"></span>
+											<a href="#">añadir <span>al </span> carro</a>
+										</p>
+										<p class="arrival-review pull-right">
+											<span class="lnr lnr-heart"></span>
+											<span class="lnr lnr-frame-expand"></span>
+										</p>
+									</div>
+								</div>
+								<h4><a href="#">Adidas predator</a></h4>
+								<p class="arrival-product-price">$1300.00</p>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-4">
+							<div class="single-new-arrival">
+								<div class="single-new-arrival-bg">
+									<img src="assets/images/slider/nike.jpg" alt="new-arrivals images">
+									<div class="single-new-arrival-bg-overlay"></div>
+									
+									<div class="new-arrival-cart">
+										<p>
+											<span class="lnr lnr-cart"></span>
+											<a href="#">añadir <span>al </span> carro</a>
+										</p>
+										<p class="arrival-review pull-right">
+											<span class="lnr lnr-heart"></span>
+											<span class="lnr lnr-frame-expand"></span>
+										</p>
+									</div>
+								</div>
+								<h4><a href="#">nike pro elite</a></h4>
+								<p class="arrival-product-price">$1800.00</p>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-4">
+							<div class="single-new-arrival">
+								<div class="single-new-arrival-bg">
+									<img src="assets/images/slider/pirma.jpg" alt="new-arrivals images">
+									<div class="single-new-arrival-bg-overlay"></div>
+									<div class="new-arrival-cart">
+										<p>
+											<span class="lnr lnr-cart"></span>
+											<a href="#">añadir <span>al </span> carro</a>
+										</p>
+										<p class="arrival-review pull-right">
+											<span class="lnr lnr-heart"></span>
+											<span class="lnr lnr-frame-expand"></span>
+										</p>
+									</div>
+								</div>
+								<h4><a href="#">pirma</a></h4>
+								<p class="arrival-product-price">$1400.00</p>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-4">
+							<div class="single-new-arrival">
+								<div class="single-new-arrival-bg">
+									<img src="assets/images/slider/adidas.jpg" alt="new-arrivals images">
+									<div class="single-new-arrival-bg-overlay"></div>
+									
+									<div class="new-arrival-cart">
+										<p>
+											<span class="lnr lnr-cart"></span>
+											<a href="#">añadir <span>al </span> carro</a>
+										</p>
+										<p class="arrival-review pull-right">
+											<span class="lnr lnr-heart"></span>
+											<span class="lnr lnr-frame-expand"></span>
+										</p>
+									</div>
+								</div>
+								<h4><a href="#">Adidas super flast</a></h4>
+								<p class="arrival-product-price">$1300.00</p>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-4">
+							<div class="single-new-arrival">
+								<div class="single-new-arrival-bg">
+									<img src="assets/images/collection/arrivals5.png" alt="new-arrivals images">
+									<div class="single-new-arrival-bg-overlay"></div>
+									<div class="new-arrival-cart">
+										<p>
+											<span class="lnr lnr-cart"></span>
+											<a href="#">añadir <span>al </span> carro</a>
+										</p>
+										<p class="arrival-review pull-right">
+											<span class="lnr lnr-heart"></span>
+											<span class="lnr lnr-frame-expand"></span>
+										</p>
+									</div>
+								</div>
+								<h4><a href="#">Silla</a></h4>
+								<p class="arrival-product-price">$1200.00</p>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-4">
+							<div class="single-new-arrival">
+								<div class="single-new-arrival-bg">
+									<img src="assets/images/collection/arrivals6.png" alt="new-arrivals images">
+									<div class="single-new-arrival-bg-overlay"></div>
+									
+									<div class="new-arrival-cart">
+										<p>
+											<span class="lnr lnr-cart"></span>
+											<a href="#">añadir <span>al </span> carro</a>
+										</p>
+										<p class="arrival-review pull-right">
+											<span class="lnr lnr-heart"></span>
+											<span class="lnr lnr-frame-expand"></span>
+										</p>
+									</div>
+								</div>
+								<h4><a href="#">comedor de madera</a></h4>
+								<p class="arrival-product-price">$2400.00</p>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-4">
+							<div class="single-new-arrival">
+								<div class="single-new-arrival-bg">
+									<img src="assets/images/collection/arrivals7.png" alt="new-arrivals images">
+									<div class="single-new-arrival-bg-overlay"></div>
+									
+									<div class="new-arrival-cart">
+										<p>
+											<span class="lnr lnr-cart"></span>
+											<a href="#">añadir <span>al </span> carro</a>
+										</p>
+										<p class="arrival-review pull-right">
+											<span class="lnr lnr-heart"></span>
+											<span class="lnr lnr-frame-expand"></span>
+										</p>
+									</div>
+								</div>
+								<h4><a href="#">sofa</a></h4>
+								<p class="arrival-product-price">$1500.00</p>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-4">
+							<div class="single-new-arrival">
+								<div class="single-new-arrival-bg">
+									<img src="assets/images/collection/arrivals8.png" alt="new-arrivals images">
+									<div class="single-new-arrival-bg-overlay"></div>
+									<div class="new-arrival-cart">
+										<p>
+											<span class="lnr lnr-cart"></span>
+											<a href="#">añadir <span>al </span> carro</a>
+										</p>
+										<p class="arrival-review pull-right">
+											<span class="lnr lnr-heart"></span>
+											<span class="lnr lnr-frame-expand"></span>
+										</p>
+									</div>
+								</div>
+								<h4><a href="#">cama matrimonial</a></h4>
+								<p class="arrival-product-price">$3200.00</p>
+							</div>
+						</div>
+					</div>
+
+					<div id="scroll-Top">
+				<div class="return-to-top">
+					<i class="fa fa-angle-up " id="scroll-top" data-toggle="tooltip" data-placement="top" title="" data-original-title="Back to Top" aria-hidden="true"></i>
+				</div>
+
+				</div>
+			</div><!--/.container-->
+
+		</section><!--/.new-arrivals-->
+		
+			</div>
+			
+        </footer>
+
+		<script src="assets/js/jquery.js"></script>
+        
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+		
+        <script src="assets/js/bootstrap.min.js"></script>
+		
+		<script src="assets/js/bootsnav.js"></script>
+
+        <script src="assets/js/owl.carousel.min.js"></script>
+
+
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+		
+        
+        <script src="assets/js/custom.js"></script>
+        
+    </body>
+	
+</html>
